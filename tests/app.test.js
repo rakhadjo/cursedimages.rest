6d9e1d8c19954a992808e911e0339a1fb66b5989
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2021-05-21 08:07:37 +0700

    add test script

---
+++ tests/app.test.js
@@ -0,0 +1,15 @@
+const { expect, afterAll } = require("@jest/globals");
+const request = require("supertest");
+const app = require("../app");
+
+describe("GET /JSON", () => {
+  it("should return json format", (done) => {
+    request(app).get("/json").expect(200, done);
+  });
+});
+
+describe("GET", () => {
+  it("should return 200", (done) => {
+    request(app).get("/").expect(200, done);
+  });
+});
