cfdf5ea4be35d03c80e285a0e731e4d2fcfddb05
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2021-05-21 10:24:33 +0700

    use env vars & fetch api

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 const port = process.env.PORT || 3000;
 
 const images = require("./links.json");
+
+var obj;
+
+fetch(process.env.rsc)
+  .then((res) => res.json())
+  .then((data) => (obj = data))
+  .then(() => console.log(data))
+  .catch((err) => {console.log(err)});
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -21,4 +30,4 @@ let server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-module.exports = server
+module.exports = server;
